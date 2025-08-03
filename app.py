5a40ed52954669e010b4fa7186d8f770dbb9fe5e
Hope Wasonga <hopeyunia@gmail.com>
2025-08-03 23:05:55 +0300

    WIP: latest changes from hope

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ api.add_resource(ProductListResource, "/api/products")
 
 # ==== Entry Point ====
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
